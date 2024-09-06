5e91b509ae9c6edf9f743095e7e7c19b6bb7cfdc
Atarian-ByVoid <cgomespimentel666@gmail.com>
2024-09-06 14:16:27 -0300

    style

--- src/main/java/com/zephry/CloudGatewayApplication.java
+++ src/main/java/com/zephry/CloudGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.zephry.cloud_gateway;
+package com.zephry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,8 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class CloudGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGatewayApplication.class, args);
+    }
 
 }
--- src/main/java/com/zephry/config/SpringCloudConfiguration.java
+++ src/main/java/com/zephry/config/SpringCloudConfiguration.java
@@ -1,4 +1,4 @@
-package com.zephry.cloud_gateway.config;
+package com.zephry.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,13 +10,9 @@ public class SpringCloudConfiguration {
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("invoiceModule", r -> r.path("/invoice/**")
-                        .uri("lb://invoice-service")
-                )
-
-                .route("customerModule", r -> r.path("/customer/**")
-                        .uri("lb://customer-service")
-                )
+                .route("syntetagmenes", r -> r.path("/heatmap/**").uri("lb://hermes-navigator"))
+                .route("atlas", r -> r.path("/map-population/**").uri("lb://hermes-navigator"))
+                .route("copernicus", r -> r.path("/temperature-map/**").uri("lb://hermes-navigator"))
                 .build();
     }
 }
