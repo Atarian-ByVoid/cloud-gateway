ab1c7b882bbe61ed32f7c0932d462529e5035174
Atarian-ByVoid <cgomespimentel666@gmail.com>
2024-09-14 12:28:50 -0300

    feat: Novas rotas adicionadas

--- src/main/java/com/zephry/config/SpringCloudConfiguration.java
+++ src/main/java/com/zephry/config/SpringCloudConfiguration.java
@@ -10,9 +10,18 @@ public class SpringCloudConfiguration {
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
+
+                // HERMES NAVIGADOR MS
                 .route("syntetagmenes", r -> r.path("/heatmap/**").uri("lb://hermes-navigator"))
                 .route("atlas", r -> r.path("/map-population/**").uri("lb://hermes-navigator"))
                 .route("copernicus", r -> r.path("/temperature-map/**").uri("lb://hermes-navigator"))
+
+                //DIMIOURGIA MS
+                .route("swagger-ui", r -> r.path("/dimiourgia/swagger-ui/**").uri("lb://dimiourgia"))
+				.route("swagger-resources", r -> r.path("/dimiourgia/swagger-resources/**").uri("lb://dimiourgia"))
+				.route("v3", r -> r.path("/dimiourgia/v3/**").uri("lb://dimiourgia"))
+                .route("user", r -> r.path("/user/**").uri("lb://dimiourgia"))
+                .route("auth", r -> r.path("/auth/**").uri("lb://dimiourgia"))
                 .build();
     }
 }
